1f01baf87708d7b3dd3d66c2fbcc66f15d19036a
inosha <inoshaonline@gmail.com>
2024-01-06 20:18:23 +0200

    Update 4.2.FizzBuzz.cs

--- Evaluate Boolean Expressions to Make Decisions in C#/4.2.FizzBuzz.cs
+++ Evaluate Boolean Expressions to Make Decisions in C#/4.2.FizzBuzz.cs
@@ -1,3 +1,10 @@
+/*
+Output values from 1 to 100, one number per line, inside the code block of an iteration statement.
+When the current value is divisible by 3, print the term Fizz next to the number.
+When the current value is divisible by 5, print the term Buzz next to the number.
+When the current value is divisible by both 3 and 5, print the term FizzBuzz next to the number.
+*/
+
 for (int i = 0; i <= 100; i++)
 {
     if (i % 5 == 0 && i % 3 == 0)
